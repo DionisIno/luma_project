0c56d1a6a393a92673ed5b428494fd68ca17ee47
Alina M <crlin.m@gmail.com>
2023-05-26 17:22:15 +0600

    test2 smth

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,2 +1,11 @@
+
+BASE_URL = "https://magento.softwaretestingboard.com/"
+
+
 def test_example(driver):
     driver.get("https://magento.softwaretestingboard.com/")
+
+
+def test_check_page_title(driver):
+    driver.get(BASE_URL)
+    assert driver.title == 'Home Page'
