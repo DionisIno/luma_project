1da747d7887e26c24651f07c09724ee77e0fb15c
Elena <peregovorun@gmail.com>
2023-05-26 16:16:07 +0300

    add test_2_page_open

--- tests/test_header.py
+++ tests/test_header.py
@@ -9,3 +9,7 @@ def test_example(driver):
 def test_check_page_title(driver):
     driver.get(BASE_URL)
     assert driver.title == 'Home Page'
+
+
+def test_2_page_open(driver):
+    driver.get("https://magento.softwaretestingboard.com/women.html")
