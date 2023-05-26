2b96eebfb8ff0815fa15ecf8c8e910dd5a18735a
Denis Tadzhibaev <incinerator88@mail.ru>
2023-05-26 17:08:53 +0300

    feat: change test_header.py

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 
 BASE_URL = "https://magento.softwaretestingboard.com/"
 
@@ -9,3 +10,10 @@ def test_example(driver):
 def test_check_page_title(driver):
     driver.get(BASE_URL)
     assert driver.title == 'Home Page'
+
+
+def test_check_link(driver):
+    driver.get("https://magento.softwaretestingboard.com/")
+    link = driver.find_element(By.CSS_SELECTOR, "li[class^='level0 nav-1']")
+    assert link.is_enabled()
+
