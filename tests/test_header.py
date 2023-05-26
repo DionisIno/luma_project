03b0054ae38ba25165b12122b3e6322afdd20541
Denis Tadzhibaev <109044090+DionisIno@users.noreply.github.com>
2023-05-26 18:21:50 +0300

    refactor: add example test (#9)
     refactor: add test_footer.py

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,11 +1,21 @@
+from selenium.webdriver.common.by import By
 
 BASE_URL = "https://magento.softwaretestingboard.com/"
 
 
 def test_example(driver):
     driver.get("https://magento.softwaretestingboard.com/")
+    print("1")
 
 
 def test_check_page_title(driver):
     driver.get(BASE_URL)
+    print("2")
     assert driver.title == 'Home Page'
+
+
+def test_check_link(driver):
+    driver.get("https://magento.softwaretestingboard.com/")
+    link = driver.find_element(By.CSS_SELECTOR, "li[class^='level0 nav-1']")
+    print("3")
+    assert link.is_enabled()
