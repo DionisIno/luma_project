72ec64df5efe6b26750eac4ce5a255f3b68672e9
Denis Tadzhibaev <incinerator88@mail.ru>
2023-05-26 12:25:42 +0300

    feat: add conftest.py, test_header.py

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def driver():
+    print('\nstart browser...')
+    chrome_options = Options()
+    if 'CI' in os.environ:
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        driver.set_window_size(1382, 754)
+    else:
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver.maximize_window()
+    yield driver
+    print('\nquit browser...')
+    driver.quit()
---
+++ tests/test_header.py
@@ -0,0 +1,2 @@
+def test_example(driver):
+    driver.get("https://magento.softwaretestingboard.com/")
