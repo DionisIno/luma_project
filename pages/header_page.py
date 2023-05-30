468c4e73038250e959f42aeb98d6ab52e0a4702f
Stanislav_Osipov <badlolpro@gmail.com>
2023-05-31 03:46:29 +0400

    add tc_01_01_01 (#11)

--- pages/header_page.py
+++ pages/header_page.py
@@ -4,3 +4,8 @@ from pages.base_page import BasePage
 
 class HeaderPage(BasePage):
     locators = HeaderPageLocators
+
+    def check_greeting_message(self):
+        message = self.element_is_visible(self.locators.GREETING_MESSAGE)
+        return message
+
--- tests/test_header.py
+++ tests/test_header.py
@@ -1,1 +1,13 @@
+from data.data_urls import MAIN_PAGE_URL
 from pages.header_page import HeaderPage
+
+
+class TestHeader:
+    """Check greeting message == 'Default welcome msg!' and is visible"""
+    def test_tc_01_01_01_verify_the_greeting_message_is_displayed(self, driver):
+        page = HeaderPage(driver, MAIN_PAGE_URL)
+        page.open()
+        message = page.check_greeting_message()
+        assert message is not None and message.text == "Default welcome msg!", \
+            "The greeting message is not displayed or the text is incorrect"
+
