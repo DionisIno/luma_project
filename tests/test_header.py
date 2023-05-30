f3c022cc9d0f657fd7248f40149650fa239c4290
Badx86 <stasure@gmail.com>
2023-05-31 03:34:53 +0400

    add comment and change assert

--- tests/test_header.py
+++ tests/test_header.py
@@ -3,9 +3,11 @@ from pages.header_page import HeaderPage
 
 
 class TestHeader:
-
+    """Check greeting message == 'Default welcome msg!' and is visible"""
     def test_tc_01_01_01_verify_the_greeting_message_is_displayed(self, driver):
         page = HeaderPage(driver, MAIN_PAGE_URL)
         page.open()
-        assert page.check_greeting_message(), "The greeting message is not displayed"
+        message = page.check_greeting_message()
+        assert message is not None and message.text == "Default welcome msg!", \
+            "The greeting message is not displayed or the text is incorrect"
 
