9654345ad230c787babb4857f5881ee62b3496c1
Badx86 <stasure@gmail.com>
2023-05-31 21:19:14 +0400

    add tc_01_01_03

--- pages/header_page.py
+++ pages/header_page.py
@@ -1,15 +1,21 @@
 from locators.header_page_locators import HeaderPageLocators
+from locators.create_account_page_locators import CreateAccountPageLocators
 from pages.base_page import BasePage
 
 
 class HeaderPage(BasePage):
-    locators = HeaderPageLocators
+    header_locators = HeaderPageLocators
+    create_account_locators = CreateAccountPageLocators
 
     def check_greeting_message(self):
-        message = self.element_is_visible(self.locators.GREETING_MESSAGE)
+        message = self.element_is_visible(self.header_locators.GREETING_MESSAGE)
         return message
 
-    def check_create_account_link(self):
-        link = self.element_is_visible(self.locators.CREATE_AN_ACCOUNT)
+    def check_create_account_page_link(self):
+        link = self.element_is_visible(self.header_locators.CREATE_AN_ACCOUNT)
         return link
+
+    def check_create_account_page_header(self):
+        header = self.element_is_visible(self.create_account_locators.CREATE_NEW_CUSTOMER_ACCOUNT_HEADER)
+        return header
 
