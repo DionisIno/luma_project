c34bcc0bd1c78621aadf6cc2abfabcc1bdfb7f6e
alena-krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-06-01 22:09:35 +0300

    add test_tc_02_01_05_verify_the_search_terms_link_is_visible_on_each_page_specified_in_data (#16)

---
+++ pages/footer_page.py
@@ -0,0 +1,10 @@
+from locators.footer_page_locators import FooterPageLocators
+from pages.base_page import BasePage
+
+
+class FooterPage(BasePage):
+    footer_locators = FooterPageLocators()
+
+    def check_search_terms_link_is_visible(self):
+        search_terms_link = self.element_is_visible(self.footer_locators.SEARCH_TERMS_LINK)
+        return search_terms_link.is_displayed()
--- tests/test_footer.py
+++ tests/test_footer.py
@@ -1,4 +1,8 @@
+
+import pytest
 from selenium.webdriver.common.by import By
+from data.data_urls import MAIN_PAGE_URL, DATA_1
+from pages.footer_page import FooterPage
 
 
 def test_check_link(driver):
@@ -6,3 +10,20 @@ def test_check_link(driver):
     link = driver.find_element(By.CSS_SELECTOR, "ul[class='footer links'] li:nth-child(1)")
     print("4")
     assert link.is_enabled()
+
+
+class TestFooter:
+    def test_tc_02_01_05_verify_the_search_terms_link_is_displayed(self, driver):
+        """Check search terms link is displayed"""
+        page = FooterPage(driver, MAIN_PAGE_URL)
+        page.open()
+        search_terms_link = page.check_search_terms_link_is_visible()
+        assert search_terms_link is True, "The element is not visible"
+
+
+@pytest.mark.parametrize('URL', DATA_1)
+def test_tc_02_01_05_verify_the_search_terms_link_is_visible_on_each_page_specified_in_data(driver, URL):
+    page = FooterPage(driver, url=URL)
+    page.open()
+    search_terms_link = page.check_search_terms_link_is_visible()
+    assert search_terms_link is True, "The element is not visible"
