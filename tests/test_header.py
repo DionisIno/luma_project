d32af8922a44b358678ce15d93f063229a1f2c88
Badx86 <stasure@gmail.com>
2023-06-02 06:40:49 +0400

    reverted back the asserts

--- tests/test_header.py
+++ tests/test_header.py
@@ -9,7 +9,8 @@ class TestHeader:
         page = HeaderPage(driver, MAIN_PAGE_URL)
         page.open()
         message = page.check_greeting_message()
-        assert message is not None and message.text == "Default welcome msg!"
+        assert message is not None and message.text == "Default welcome msg!", \
+            "The greeting message is not displayed or the text is incorrect"
 
     def test_tc_01_01_02_verify_the_display_and_interactivity_of_the_create_an_account_link(self, driver):
         """Check 'Create an account' link is displayed and underlined"""
@@ -17,7 +18,8 @@ class TestHeader:
         page.open()
         link = page.check_create_account_page_link()
         page.action_move_to_element(link)
-        assert link is not None and "underline" in link.value_of_css_property('text-decoration')
+        assert link is not None and "underline" in link.value_of_css_property('text-decoration'), \
+            "Link 'Create an account' is either not displayed or not underlined on hover"
 
     def test_tc_01_01_03_verify_the_correctness_create_an_account_link(self, driver):
         """Check 'Create an account' link click redirects to the account’s registration page and \
@@ -26,7 +28,8 @@ class TestHeader:
         page.open()
         page.click_and_return_element(page.header_locators.CREATE_AN_ACCOUNT)
         header = page.check_create_account_page_header()
-        assert driver.current_url == CREATE_ACCOUNT_PAGE_URL and header.text == "Create New Customer Account"
+        assert driver.current_url == CREATE_ACCOUNT_PAGE_URL and header.text == "Create New Customer Account", \
+            "Create an account page is either not opened or the page header is incorrect"
 
     def test_tc_01_01_04_verify_the_display_and_interactivity_of_the_sign_in_link(self, driver):
         """Check 'Sign In' link is displayed and underlined"""
@@ -34,7 +37,8 @@ class TestHeader:
         page.open()
         link = page.check_sign_in_page_link()
         page.action_move_to_element(link)
-        assert link is not None and "underline" in link.value_of_css_property('text-decoration')
+        assert link is not None and "underline" in link.value_of_css_property('text-decoration'), \
+            "Link 'Sign In' is either not displayed or not underlined on hover"
 
     def test_tc_01_01_05_verify_the_correctness_sign_in_link(self, driver):
         """Check 'Sign In' link click redirects to the login page and \
@@ -43,7 +47,8 @@ class TestHeader:
         page.open()
         page.click_and_return_element(page.header_locators.SIGN_IN)
         header = page.check_sign_in_page_header()
-        assert driver.current_url == SIGN_IN_URL and header.text == "Customer Login"
+        assert driver.current_url == SIGN_IN_URL and header.text == "Customer Login", \
+            "Sign In page is either not opened or the page header is incorrect"
 
     def test_tc_01_02_16_verify_the_display_and_interactivity_of_the_logo(self, driver):
         """Check that the cursor changes to a 'hand' indicating the logo is a clickable element, \
@@ -54,5 +59,6 @@ class TestHeader:
         page.action_move_to_element(logo)
         cursor_type = logo.value_of_css_property('cursor')
         page.click_and_return_element(page.header_locators.LOGO)
-        assert cursor_type == 'pointer' and driver.current_url == MAIN_PAGE_URL
-
+        assert cursor_type == 'pointer' and driver.current_url == MAIN_PAGE_URL, \
+            "Failed: Either the cursor does not change to a 'hand' when hovering over the logo or " \
+            "clicking on the logo does not redirect to the main page"
