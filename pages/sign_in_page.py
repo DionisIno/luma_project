09c148c05c17d175ff8e75afb93426feaf0b37f7
verafes <verafes@gmail.com>
2023-06-02 09:09:45 -0700

    Add: 03.01.06 SignIn > Registered Customers > email label

--- pages/sign_in_page.py
+++ pages/sign_in_page.py
@@ -12,3 +12,10 @@ class SignInPage(BasePage):
     def check_registered_customers_heading(self):
         heading = self.element_is_visible(self.locators.REGISTERED_CUSTOMERS_HEADER)
         return heading
+
+    def check_customer_email_label(self):
+        '''This method verifies if email label is visible'''
+        return self.element_is_visible(self.locators.CUSTOMER_EMAIL_LABEL)
+
+    def check_customer_email_asterisk(self):
+        return self.element_is_visible(self.locators.CUSTOMER_EMAIL_ASTERISK)
