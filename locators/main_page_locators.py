08e5fe2eab9d554f3d2991b5d56e8ddb22f9d885
Denis Tadzhibaev <109044090+DionisIno@users.noreply.github.com>
2023-06-05 18:45:44 +0300

    feat: add test_verify_the_card_is_interactive_on_hover (#44)

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -1,9 +1,12 @@
 """This section contain main page locators"""
-from random import randint
+import random
 
 from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
+
     # Hot Seller section
-    CARD_TITLE = (By.CSS_SELECTOR, f"strong[class='product-item-name']:nth-child({randint(1, 6)})")
+    CARD_TITLE = (By.CSS_SELECTOR, f"strong[class='product-item-name']:nth-child({random.randint(1, 6)})")
+    PRODUCT_CARD = (By.CSS_SELECTOR,
+                    f"li[class='product-item']:nth-child({random.randint(1, 6)}) div[class='product-item-info']")
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,8 +1,18 @@
 """This section contains home page tests"""
 
-
 from pages.main_page import MainPage
+from data.data_urls import MAIN_PAGE_URL
 
 
 class TestMainPage:
-    pass
+
+    class TestHotSellerSection:
+
+        def test_verify_the_card_is_interactive_on_hover(self, driver):
+            """This test checks that the card is interactive."""
+            page = MainPage(driver, MAIN_PAGE_URL)
+            page.open()
+            shadow_before, shadow_after = page.check_for_shadow_appearance_when_hovering_over_the_card()
+            assert shadow_before != shadow_after, \
+                "Card is not interactive"
+
