b4828bc0ac63c040cfd22b7ba8616c12c60b9aeb
Denis Tadzhibaev <incinerator88@mail.ru>
2023-06-05 18:41:30 +0300

    feat: add test_verify_the_card_is_interactive_on_hover

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 """This section contains the basic steps for running homepage tests"""
+
 from locators.main_page_locators import MainPageLocators
 from pages.base_page import BasePage
 
@@ -6,3 +7,10 @@ from pages.base_page import BasePage
 class MainPage(BasePage):
     locators = MainPageLocators
 
+    def check_for_shadow_appearance_when_hovering_over_the_card(self):
+        """Checks for shadows to appear when looking at a card"""
+        card_item = self.element_is_visible(self.locators.PRODUCT_CARD)
+        shadow_before = card_item.value_of_css_property("box-shadow")
+        self.action_move_to_element(card_item)
+        shadow_after = card_item.value_of_css_property("box-shadow")
+        return shadow_before, shadow_after
