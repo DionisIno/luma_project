a15b7b865aeedc63ca3a9019e47d1731d4da7ee5
alena-krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-06-05 20:02:26 +0300

    add test_tc_02_01_01_uniform_footer_elements_display_link_is_visible_on_each_page_specified_in_data (#46)

--- pages/footer_page.py
+++ pages/footer_page.py
@@ -8,3 +8,7 @@ class FooterPage(BasePage):
     def check_search_terms_link_is_visible(self):
         search_terms_link = self.element_is_visible(self.footer_locators.SEARCH_TERMS_LINK)
         return search_terms_link.is_displayed()
+
+    def some_link_is_visible(self):
+        search_terms_link = self.element_is_visible(self.footer_locators.DATA_2)
+        return search_terms_link.is_displayed()
