27e318803a55b0235f1f25badbaebb5065064c24
Elena <peregovorun@gmail.com>
2023-06-05 19:55:40 +0300

    add test_tc_02_01_01_uniform_footer_elements_display_link_is_visible_on_each_page_specified_in_data

--- locators/footer_page_locators.py
+++ locators/footer_page_locators.py
@@ -1,8 +1,26 @@
-"""This section contain footer locators"""
+"""This section contains footer locators"""
 
 from selenium.webdriver.common.by import By
 
 
 class FooterPageLocators:
+    ADVANCED_SEARCH_LINK = (By.CSS_SELECTOR, '.footer > :nth-child(3) > a')
+    CONTACT_US_LINK = (By.CSS_SELECTOR, '.footer > :nth-child(5) > a')
+    COPYRIGHT_TEXT = (By.CSS_SELECTOR, '.copyright > span')
+    FOOTER_SECTION = (By.CSS_SELECTOR, '.page-footer')
+    FOOTER_CONTENT = (By.CSS_SELECTOR, '.footer.content')
+    ORDERS_AND_RETURNS = (By.CSS_SELECTOR, '.footer > :nth-child(4) > a')
+    PRIVACY_AND_COOKIE_POLICY_LINK = (By.CSS_SELECTOR, '.footer > :nth-child(2) > a')
     SEARCH_TERMS_LINK = (By.CSS_SELECTOR, "ul[class='footer links'] li:nth-child(1)")
+    SUBSCRIBE_BLOCK = (By.CSS_SELECTOR, 'div.block.newsletter')
+    SUBSCRIBE_BUTTON = (By.CSS_SELECTOR, '#newsletter-validate-detail > .actions > .action')
+    SUBSCRIBE_BUTTON_TEXT = (By.CSS_SELECTOR, '#newsletter-validate-detail > .actions > .action > span')
+    SUBSCRIBE_EMAIL_FIELD = (By.CSS_SELECTOR, '#newsletter')
+    SUBSCRIBE_EMAIL_FIELD_ICON = (By.CSS_SELECTOR, "div[class='control']")
+    WRITE_FOR_US_LINK = (By.CSS_SELECTOR, '.widget > ul > li > a')
+
+    DATA_2 = [
+        "ADVANCED_SEARCH_LINK = (By.CSS_SELECTOR, '.footer > :nth-child(3) > a')",
+        " CONTACT_US_LINK = (By.CSS_SELECTOR, '.footer > :nth-child(5) > a')"
+    ]
     
--- tests/test_footer.py
+++ tests/test_footer.py
@@ -2,20 +2,27 @@ import pytest
 from selenium.webdriver.common.by import By
 from data.data_urls import MAIN_PAGE_URL, DATA_1
 from pages.footer_page import FooterPage
+from locators.footer_page_locators import FooterPageLocators
 
 
 class TestFooter:
-    def test_tc_02_01_05_verify_the_search_terms_link_is_displayed(self, driver):
-        """Check search terms link is displayed"""
-        page = FooterPage(driver, MAIN_PAGE_URL)
+
+    @pytest.mark.parametrize('URL', DATA_1)
+    def test_tc_02_01_05_verify_the_search_terms_link_is_visible_on_each_page_specified_in_data(self, driver, URL):
+        """Check search terms link is displayed on each page in DATA_1"""
+        page = FooterPage(driver, url=URL)
         page.open()
         search_terms_link = page.check_search_terms_link_is_visible()
         assert search_terms_link is True, "The element is not visible"
 
 
-@pytest.mark.parametrize('URL', DATA_1)
-def test_tc_02_01_05_verify_the_search_terms_link_is_visible_on_each_page_specified_in_data(driver, URL):
-    page = FooterPage(driver, url=URL)
-    page.open()
-    search_terms_link = page.check_search_terms_link_is_visible()
-    assert search_terms_link is True, "The element is not visible"
+class TestFooterSuits:
+
+    @pytest.mark.parametrize('element', FooterPageLocators.DATA_2)
+    @pytest.mark.parametrize('URL', DATA_1)
+    def test_tc_02_01_01_uniform_footer_elements_display_on_each_page_specified_in_data(self, driver, URL, element):
+        """Check search footer elements in DATA_2 are displayed on each page in DATA_1"""
+        page = FooterPage(driver, url=URL)
+        page.open()
+        search_terms_link = page.check_search_terms_link_is_visible()
+        assert search_terms_link is True, "The element is not visible"
