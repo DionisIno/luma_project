909d57ec4d9e02f8f9b7e607a48cc870098bdc33
NKrap <129363326+NKrap@users.noreply.github.com>
2023-06-09 15:24:25 +0500

    Added Bags link functionality test (#66)

--- tests/test_gear.py
+++ tests/test_gear.py
@@ -1,6 +1,6 @@
 from data.data_urls import GEAR_PAGE_URL
+from data.gear_data import expected_link
 from pages.gear_page_ import GearPage
-
 
 
 class TestGearPage:
@@ -12,4 +12,10 @@ class TestGearPage:
         bags = page.check_bags_element()
         assert bags is not None, "The element isn't displayed or not enabled"
 
+    def test_TC_11_01_03(self, driver):
+        page = GearPage(driver, GEAR_PAGE_URL)
+        page.open()
+        page.check_bags_functionality()
+        assert page.get_actual_url(driver) == expected_link['gear_url'], \
+            'The link is not correct'
 
