6df5ae6f7b626f309d70f99079f3e0fcae20c734
NKrap <129363326+nkrap@users.noreply.github.com>
2023-06-09 15:24:25 +0500

    Added test for checking title of the Category section

--- pages/gear_page_.py
+++ pages/gear_page_.py
@@ -7,8 +7,20 @@ from pages.base_page import BasePage
 class GearPage(BasePage):
     locators = SideBarLocators
 
+    def check_text_in_category_title(self):
+        category_title = self.element_is_visible(self.locators.CATEGORY)
+        title_category = category_title.text
+        return title_category
+
     def check_bags_element(self):
         bags = self.element_is_clickable(self.locators.BAGS)
         return bags
 
+    def check_bags_functionality(self):
+        bags = self.element_is_visible(self.locators.BAGS)
+        link = bags.click()
+        return link
 
+    def get_actual_url(self, driver):
+        actual_url = driver.current_url
+        return actual_url
