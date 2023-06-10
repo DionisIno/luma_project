1531e2ab7d41b04331ef6649907e7e4ade72d84f
ILLIA LEVCHENKO <illia.levchenko.work@gmail.com>
2023-06-10 10:27:00 -0700

    add +1 time.sleep(1)

--- pages/reviews_page.py
+++ pages/reviews_page.py
@@ -8,11 +8,13 @@ class ReviewsPage(BasePage):
 
     def one_star_review_correct(self):
         one_star_element = self.element_is_clickable(self.locators.STAR_1)
+        time.sleep(1)
         one_star_element.click()
         time.sleep(1)
 
     def nickname_input_review_correct(self):
         nickname_input_element = self.element_is_clickable(self.locators.NICKNAME_INPUT)
+        time.sleep(1)
         nickname_input_element.click()
         nickname_input_element.clear()
         nickname_input_element.send_keys('Some Name')
@@ -20,6 +22,7 @@ class ReviewsPage(BasePage):
 
     def summary_input_review_correct(self):
         summary_input_element = self.element_is_clickable(self.locators.SUMMARY_INPUT)
+        time.sleep(1)
         summary_input_element.click()
         summary_input_element.clear()
         summary_input_element.send_keys('Some Summary Text')
@@ -27,6 +30,7 @@ class ReviewsPage(BasePage):
 
     def review_input_review_correct(self):
         review_input_element = self.element_is_clickable(self.locators.REVIEW_INPUT)
+        time.sleep(1)
         review_input_element.click()
         review_input_element.clear()
         review_input_element.send_keys('Some Review Text')
@@ -34,6 +38,7 @@ class ReviewsPage(BasePage):
 
     def send_review_correct(self):
         review_input_element = self.element_is_visible(self.locators.SUBMIT_REVIEW_BUTTON)
+        time.sleep(1)
         review_input_element.click()
         review_successfully_submitted = self.element_is_visible(self.locators.REVIEW_SUCCESSFULLY_SUBMITTED)
         time.sleep(1)
