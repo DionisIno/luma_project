37a6d8168377ef86deb2532d6381f5f21e300890
ILLIA LEVCHENKO <illia.levchenko.work@gmail.com>
2023-06-10 11:15:23 -0700

    Вернул element_is_present timeout=15 => 5, Убрал тайм-слипы из review_page.py

--- pages/reviews_page.py
+++ pages/reviews_page.py
@@ -22,25 +22,18 @@ class ReviewsPage(BasePage):
 
     def summary_input_review_correct(self):
         summary_input_element = self.element_is_clickable(self.locators.SUMMARY_INPUT)
-        time.sleep(1)
         summary_input_element.click()
         summary_input_element.clear()
         summary_input_element.send_keys('Some Summary Text')
-        time.sleep(1)
 
     def review_input_review_correct(self):
         review_input_element = self.element_is_clickable(self.locators.REVIEW_INPUT)
-        time.sleep(1)
         review_input_element.click()
         review_input_element.clear()
         review_input_element.send_keys('Some Review Text')
-        time.sleep(1)
 
     def send_review_correct(self):
         review_input_element = self.element_is_visible(self.locators.SUBMIT_REVIEW_BUTTON)
-        time.sleep(1)
         review_input_element.click()
-        time.sleep(1)
         review_successfully_submitted = self.element_is_present(self.locators.REVIEW_SUCCESSFULLY_SUBMITTED)
-        time.sleep(1)
         return review_successfully_submitted
