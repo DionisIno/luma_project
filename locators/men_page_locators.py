4281906a505a890f439b1c51a0553434006a404c
Galina Danilova <galya.danilova@gmail.com>
2023-06-10 23:56:12 -0400

    add test_14_02_02_verify_tops_link_is_visible_and_clickable (#93)

---
+++ locators/men_page_locators.py
@@ -0,0 +1,7 @@
+"""This section contain Men page locators"""
+
+from selenium.webdriver.common.by import By
+
+
+class MenPageLocators:
+    SIDE_BAR_TOPS = (By.CSS_SELECTOR, '#narrow-by-list2 a:first-child')
