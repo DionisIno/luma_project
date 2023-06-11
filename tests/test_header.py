f0cfcc97a6cbdd7977fdefd0e617eb57b2b28646
Badx86 <email@gmail.com>
2023-06-11 05:06:42 +0400

    change TC numbers

--- tests/test_header.py
+++ tests/test_header.py
@@ -46,7 +46,7 @@ class TestHeader:
         assert driver.current_url == SIGN_IN_URL and header.text == "Customer Login", \
             "Sign In page is either not opened or the page header is incorrect"
 
-    def test_tc_01_02_16_display_and_interactivity_of_the_logo(self, driver):
+    def test_tc_01_02_04_display_and_interactivity_of_the_logo(self, driver):
         """Check that the cursor changes to a 'hand' indicating the logo is a clickable element, \
         and that clicking on it causes the page to reload and redirect to the main page"""
         page = HeaderPage(driver, MAIN_PAGE_URL)
@@ -61,7 +61,7 @@ class TestHeader:
         link = page.redirected_the_link_sale()
         assert link, "Sale page isn't opened or the page header is incorrect"
 
-    def test_tc_01_02_17_display_and_interactivity_of_the_cart_icon(self, driver):
+    def test_tc_01_02_05_display_and_interactivity_of_the_cart_icon(self, driver):
         """Check hovering over the shopping cart icon changes the cursor to a "hand," indicating that the cart is a \
         clickable element, click on it and the message "You have no items in your shopping cart." appears"""
         page = HeaderPage(driver, MAIN_PAGE_URL)
@@ -72,21 +72,21 @@ class TestHeader:
         assert "You have no items in your shopping cart." in page.check_cart_message(), \
             "Failed: The message in the cart is not correct or the cart is not empty"
 
-    def test_tc_01_02_15_presence_of_a_placeholder_in_the_search_field(self, driver):
+    def test_tc_01_02_03_presence_of_a_placeholder_in_the_search_field(self, driver):
         """Check the "Search" field in the header contains the placeholder 'Search entire store here...'"""
         page = HeaderPage(driver, MAIN_PAGE_URL)
         page.open()
         assert page.check_search_field() == "Search entire store here...", \
             "Text in the Placeholder field does not match or is missing"
 
-    def test_tc_01_02_14_functionality_of_the_search_field(self, driver):
+    def test_tc_01_02_02_functionality_of_the_search_field(self, driver):
         """Check the "Search" field is activated and the drop-down list appears with various product"""
         page = HeaderPage(driver, MAIN_PAGE_URL)
         page.open()
         dropdown_items = page.enter_search_field_and_get_dropdown()
         assert dropdown_items is not None and len(dropdown_items) > 0, "Error: No drop-down list appears or it`s empty"
 
-    def test_tc_01_02_13_display_and_interactivity_of_the_search_field(self, driver):
+    def test_tc_01_02_01_display_and_interactivity_of_the_search_field(self, driver):
         """Check the "Search" field is activated with a vertical flashing symbol "|" and highlighted"""
         page = HeaderPage(driver, MAIN_PAGE_URL)
         page.open()
