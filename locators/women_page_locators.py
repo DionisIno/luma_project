a19ac180e23a129d089ee706bf2523696d913a39
Aleksei Kanikovskii <Vrashfor@gmail.com>
2023-06-12 03:37:15 +0300

    Added Category sign visibility test (#114)

--- locators/women_page_locators.py
+++ locators/women_page_locators.py
@@ -7,3 +7,4 @@ class WomenPageLocators:
     # Side panel | Shop By
     SHOP_BY = (By.XPATH, '//div[4]/div[2]/div[1]/div[1]/strong')
     WOMEN_HEAD_TEXT = (By.XPATH, '//*[@id="page-title-heading"]/span')
+    CATEGORY_TITLE = (By.XPATH, '//div[4]/div[2]/div[1]/div[2]/dl/dt')
