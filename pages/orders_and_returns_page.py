ae6e9f364df1cd7214527d89f86e413eb481d69c
Vitaly S <vitaliksew@gmail.com>
2023-06-12 11:35:39 -0400

    create: orders_and_returns_data.py in data folder add: test_tc_05_01_02_check_header, add: test_tc_05_01_03_check_order_id_field:  tests in test_orders_and_returns.py

--- pages/orders_and_returns_page.py
+++ pages/orders_and_returns_page.py
@@ -10,3 +10,11 @@ class OrdersAndReturnsPage(BasePage):
     def check_header(self):
         """Checks "Orders and Returns" header text"""
         return self.get_text(self.locators.PAGE_HEADER)
+
+    def check_form_header(self):
+        """Checks "Orders and Returns" form header text"""
+        return self.get_text(self.locators.FORM_HEADER)
+
+    def check_order_id_input_is_visible(self):
+        """Checks "Order ID" input is visible"""
+        return self.element_is_visible(self.locators.ORDER_ID_INPUT)
