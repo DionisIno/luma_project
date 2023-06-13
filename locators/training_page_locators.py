5adbc87c9bdaf20dc12c3404bbc43c65fac58ae7
Lipatnikova <aerovaya@gmail.com>
2023-06-13 13:12:15 +0400

    TC_16.01.01 Verify Sidebar panel title Shop By is visible

--- locators/training_page_locators.py
+++ locators/training_page_locators.py
@@ -6,3 +6,6 @@ from selenium.webdriver.common.by import By
 class TrainingPageLocators:
 
     HEAD_TEXT = (By.CSS_SELECTOR, '[data-ui-id="page-title-wrapper"]')
+
+    # Sidebar panel section
+    SHOP_BY = (By.XPATH, '//div[4]/div[2]/div[1]/div[1]/strong')
