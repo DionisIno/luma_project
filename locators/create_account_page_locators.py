bb4d77416dd6e8e5847db57bdcf0bc5802dd0590
Alina M <crlin.m@gmail.com>
2023-06-13 13:02:54 +0600

    create an account testcases: 04.01.01 04.01.02 04.01.03 04.01.04 04.01.05 04.01.06 04.01.07 04.01.08 04.01.09

--- locators/create_account_page_locators.py
+++ locators/create_account_page_locators.py
@@ -4,14 +4,19 @@ from selenium.webdriver.common.by import By
 
 
 class CreateAccountPageLocators:
-    # Page-title Wrapper
+    # # # Create an Account
     CREATE_AN_ACCOUNT_HEADER = (By.CSS_SELECTOR, "h1")
-    PERSONAL_INFORMATION_LEGEND = (By.CSS_SELECTOR, ".fieldset.create.info")
-    CREATE_AN_ACCOUNT_FIRSTNAME_LABEL = (By.CSS_SELECTOR, "label[for='firstname']")
-    CREATE_AN_ACCOUNT_FIRSTNAME = (By.CSS_SELECTOR, "#firstname")
-    # &&&&&&&&&&
-    CREATE_AN_ACCOUNT_FIRSTNAME_ASTERISK = (By.CSS_SELECTOR, "+ span.required")
-    CREATE_AN_ACCOUNT_LASTNAME_LABEL = (By.CSS_SELECTOR, "label[for='lastname']")
-    CREATE_AN_ACCOUNT_LASTNAME = (By.CSS_SELECTOR, "#lastname")
-    CREATE_AN_ACCOUNT_SIGN_UP_CHECKBOX = (By.CSS_SELECTOR, "label[for='is_subscribed']")
-    CREATE_AN_ACCOUNT_BUTTON = (By.CSS_SELECTOR, "button[title='Create an Account']")
+    PERSONAL_INFORMATION_LEGEND = (By.CSS_SELECTOR, ".fieldset.create.info legend")
+    FIRST_NAME_LABEL = (By.CSS_SELECTOR, "label[for='firstname']")
+    FIRST_NAME = (By.ID, 'firstname')
+    LAST_NAME_LABEL = (By.CSS_SELECTOR, "label[for='lastname']")
+    LAST_NAME = (By.ID, "lastname")
+    SIGN_UP_CHECKBOX = (By.ID, "is_subscribed")
+    SIGN_UP_CHECKBOX_LABEL = (By.CSS_SELECTOR, "label[for='is_subscribed']")
+
+    EMAIL = (By.ID, 'email_address')
+    PASSWORD = (By.CSS_SELECTOR, '[id="password"]')
+    PASSWORD_CONFIRMATION = (By.CSS_SELECTOR, '[id="password-confirmation"]')
+    CREATE_AN_ACCOUNT_BUTTON = (By.CSS_SELECTOR, 'button[title="Create an Account"]')
+    MESSAGE_ERROR = (By.CSS_SELECTOR, '.message-error.error.message > div')
+    MESSAGE_SUCCESS = (By.CSS_SELECTOR, '.message-success.success.message > div')
