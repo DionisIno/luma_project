e6e79bbe94e91e632deebc0fd0d4da8f0e0b5605
Jenny_Kadzova <kadzovaea@gmail.com>
2023-06-14 10:18:19 -0800

    add: test_tc_10_01_03__05_07_09_11_13 (#142)

--- data/sale_data.py
+++ data/sale_data.py
@@ -1,4 +1,4 @@
-expected_titles = {
+expected_titles_w = {
     "hoodies_women": "Hoodies & Sweatshirts - Tops - Women",
     "jackets_women": "Jackets - Tops - Women",
     "tees_women": "Tees - Tops - Women",
@@ -7,7 +7,7 @@ expected_titles = {
     "shorts_women": "Shorts - Bottoms - Women"
 }
 
-expected_urls = {
+expected_urls_w = {
     "hoodies_women_url": 'https://magento.softwaretestingboard.com/'
                          'women/tops-women/hoodies-and-sweatshirts-women.html',
     "jackets_women_url": 'https://magento.softwaretestingboard.com/women/tops-women/jackets-women.html',
@@ -15,4 +15,20 @@ expected_urls = {
     "bras_women_url": 'https://magento.softwaretestingboard.com/women/tops-women/tanks-women.html',
     "pants_women_url": 'https://magento.softwaretestingboard.com/women/bottoms-women/pants-women.html',
     "shorts_women_url": 'https://magento.softwaretestingboard.com/women/bottoms-women/shorts-women.html'
+}
+
+expected_titles_m = {
+    "hoodies_men": "Hoodies & Sweatshirts - Tops - Men",
+    "jackets_men": "Jackets - Tops - Men",
+    "tees_women": "Tees - Tops - Men",
+    "pants_men": "Pants - Bottoms - Men",
+    "shorts_men": "Shorts - Bottoms - Men"
+}
+
+expected_urls_m = {
+    "hoodies_men_url": 'https://magento.softwaretestingboard.com/men/tops-men/hoodies-and-sweatshirts-men.html',
+    "jackets_men_url": 'https://magento.softwaretestingboard.com/men/tops-men/jackets-men.html',
+    "tees_men_url": 'https://magento.softwaretestingboard.com/men/tops-men/tees-men.html',
+    "pants_men_url": 'https://magento.softwaretestingboard.com/men/bottoms-men/pants-men.html',
+    "shorts_men_url": 'https://magento.softwaretestingboard.com/men/bottoms-men/shorts-men.html'
 }
