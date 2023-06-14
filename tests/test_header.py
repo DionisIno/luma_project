117db06ee29187d4c0fffeb9387f7d6ba10d9714
Badx86 <badlolpro@gmail.com>
2023-06-15 01:01:55 +0400

    try4 to fix

--- tests/test_header.py
+++ tests/test_header.py
@@ -267,7 +267,8 @@ class TestHeader:
         page = HeaderPage(driver, MAIN_PAGE_URL)
         page.open()
         page.user_authorization()
-        page.element_is_visible(page.header_locators.DROPDOWN_BUTTON)
+        # page.element_is_visible(page.header_locators.DROPDOWN_BUTTON)
+        page.action_move_to_element(page.element_is_visible(page.header_locators.DROPDOWN_BUTTON))
         assert page.element_is_visible(page.header_locators.DROPDOWN_BUTTON), \
             "Error: dropdown button is not visible"
 
