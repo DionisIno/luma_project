28596215282865733627a44adae943ccf0c16b57
Jenny_Kadzova <kadzovaea@gmail.com>
2023-06-14 09:36:56 -0800

    add: test_tc_10_02_02__04__06__08__10 (#141)

--- tests/test_sale.py
+++ tests/test_sale.py
@@ -9,7 +9,6 @@ from locators.sale_page_locators import SideBarLocators
 
 @allure.epic("Sale Page")
 class TestSalePage:
-
     @allure.feature("Testing Side Menu Bar - Women's Deals Section")
     class TestSideMenuBarWomen:
         @allure.title("TC 10.01.01 - Verify 'WOMEN'S DEALS' title is correct")
@@ -61,3 +60,13 @@ class TestSalePage:
             page.open()
             title = page.check_text_in_men_deals_title()
             assert title == "MEN'S DEALS", f"Expected title: 'MEN'S DEALS', Actual title: {title}"
+
+        @allure.title("TC 10.02.02, 10.02.04, 10.02.06, 10.02.08, 10.02.10 - "
+                      "Verify 5 links in Men's Deals section are visible and clickable")
+        @pytest.mark.parametrize("element_locator", SideBarLocators.MEN_DEALS_ELEMENTS.values())
+        def test_tc_10_02_02__04__06__08__10(self, driver, element_locator):
+            """Check that five elements in Men's Deals section are displayed and enabled"""
+            page = SalePage(driver, SALE_PAGE_URL)
+            page.open()
+            element_in_men_deals = page.element_is_clickable(element_locator)
+            assert element_in_men_deals is not None, "Element is not displayed or enabled"
