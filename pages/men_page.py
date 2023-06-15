6459108eac844f10756e2d077cc615a1451293c1
gdanilova <130004322+gdanilova@users.noreply.github.com>
2023-06-14 20:52:24 -0400

    add test TC 14.02.05 Verify the link 'Bottoms' redirects to a correct page.

--- pages/men_page.py
+++ pages/men_page.py
@@ -1,4 +1,4 @@
-from data.data_urls import MEN_TOPS_URL
+from data.data_urls import MEN_TOPS_URL, MEN_BOTTOMS_URL
 from locators.men_page_locators import MenPageLocators
 from pages.base_page import BasePage
 from selenium.webdriver.support.ui import WebDriverWait as wait
@@ -29,3 +29,12 @@ class MenPage(BasePage):
         """This method finds 'Bottoms' link and verifies it is clickable"""
         self.element_is_clickable(self.side_bar_locators.SIDE_BAR_BOTTOMS)
         return wait(self.driver, 5).until(EC.element_to_be_clickable(self.side_bar_locators.SIDE_BAR_BOTTOMS))
+
+    @allure.step('Correct redirection of the link "Bottoms" on Men page')
+    def verify_tops_bottoms_redirects_to_a_correct_page(self):
+        """This method finds 'Bottoms' link and verifies it is correctly redirects to a new page"""
+        element = self.element_is_visible(self.side_bar_locators.SIDE_BAR_BOTTOMS)
+        element.click()
+        url = self.driver.current_url
+        text = self.get_text(self.side_bar_locators.MEN_SUBHEAD_TEXT_BOTTOMS)
+        return url == MEN_BOTTOMS_URL and text == "Bottoms"
