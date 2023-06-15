87b0f3f30fcdc6ad32b8a501153cd53b58ba3abe
Lipatnikova Anastasiya <106307423+Lipatnikova@users.noreply.github.com>
2023-06-15 14:31:37 +0400

    add: TC_16.01.02 - TC_16.01.06 Verify Sidebar panel titles category, video download, compare products, my wish list are visible and contains correct text (#152)

--- locators/training_page_locators.py
+++ locators/training_page_locators.py
@@ -9,3 +9,7 @@ class TrainingPageLocators:
 
     # Sidebar panel section
     SHOP_BY = (By.XPATH, '//div[4]/div[2]/div[1]/div[1]/strong')
+    CATEGORY = (By.XPATH, '//*[@id="narrow-by-list2"]/dt')
+    VIDEO_DOWNLOAD = (By.XPATH, '//*[@id="narrow-by-list2"]/dd/ol/li/a')
+    COMPARE_PRODUCTS = (By.XPATH, '//*[@id="block-compare-heading"]')
+    MY_WISH_LIST = (By.XPATH, '//*[@id="maincontent"]/div[4]/div[3]/div[2]/div[1]/strong')
--- pages/training_page.py
+++ pages/training_page.py
@@ -7,8 +7,32 @@ import allure
 class TrainingPage(BasePage):
     locators = TrainingPageLocators
 
-    @allure.step('Check the Sidebar Shop By title is visible')
-    def check_sidebar_panel_name(self):
-        shop_by_text = self.element_is_visible(self.locators.SHOP_BY)
-        return shop_by_text.text
+    @allure.step('Check the Sidebar Shop By title is visible and contains text "Shop By"')
+    def check_sidebar_panel_shop_by(self):
+        shop_by = self.element_is_visible(self.locators.SHOP_BY)
+        return shop_by.text
 
+    @allure.step('Check the Sidebar Category title is visible and contains text "Category"')
+    def check_sidebar_panel_category(self):
+        category = self.element_is_visible(self.locators.CATEGORY)
+        return category.text
+
+    @allure.step('Check the Sidebar Video Download is visible and contains text "Video Download"')
+    def check_sidebar_panel_video_download(self):
+        video_download = self.element_is_visible(self.locators.VIDEO_DOWNLOAD)
+        return video_download.text
+
+    @allure.step('Check the Sidebar Video Download is visible and contains URL')
+    def check_sidebar_panel_video_download_href(self):
+        video_download = self.element_is_visible(self.locators.VIDEO_DOWNLOAD)
+        return video_download.get_attribute("href")
+
+    @allure.step('Check the Sidebar Compare Products is visible and contains text "Compare Products"')
+    def check_sidebar_panel_compare_products(self):
+        compare_products = self.element_is_visible(self.locators.COMPARE_PRODUCTS)
+        return compare_products.text
+
+    @allure.step('Check the Sidebar My Wish List is visible and contains text "My Wish List"')
+    def check_sidebar_panel_my_wish_list(self):
+        my_wish_list = self.element_is_visible(self.locators.MY_WISH_LIST)
+        return my_wish_list.text
