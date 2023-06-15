03716008667638288197655cb6b979397ab45726
Galina Danilova <galya.danilova@gmail.com>
2023-06-14 23:46:58 -0400

    add test TC 14.03.01 Verify the 'TOPS' subhead is displayed (#150)

--- locators/men_page_locators.py
+++ locators/men_page_locators.py
@@ -8,3 +8,5 @@ class MenPageLocators:
     MEN_SUBHEAD_TEXT_TOPS = (By.CSS_SELECTOR, '#page-title-heading')
     SIDE_BAR_BOTTOMS = (By.CSS_SELECTOR, '#narrow-by-list2 > dd > ol > li:nth-child(2) > a')
     MEN_SUBHEAD_TEXT_BOTTOMS = (By.CSS_SELECTOR, '#page-title-heading > span')
+    SIDE_BAR_SUBHEAD_TOPS = (By.XPATH, '//*[@id="maincontent"]/div[4]/div[2]/div[2]/div/strong/span')
+    SIDE_BAR_SUBHEAD_BOTTOMS = (By.CSS_SELECTOR, '#maincontent > div.sidebar > div.widget > div > strong:nth-child(3)')
