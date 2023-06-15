dfc17dd8e0ec9184856b20ac5e909d05f80f9ae4
gdanilova <130004322+gdanilova@users.noreply.github.com>
2023-06-14 20:16:43 -0400

    TC 14.02.04 Verify the link 'Bottoms' is visible and clickable.

--- locators/men_page_locators.py
+++ locators/men_page_locators.py
@@ -6,3 +6,4 @@ from selenium.webdriver.common.by import By
 class MenPageLocators:
     SIDE_BAR_TOPS = (By.CSS_SELECTOR, '#narrow-by-list2 a:first-child')
     MEN_SUBHEAD_TEXT_TOPS = (By.CSS_SELECTOR, '#page-title-heading')
+    SIDE_BAR_BOTTOMS = (By.CSS_SELECTOR, '#narrow-by-list2 > dd > ol > li:nth-child(2) > a')
