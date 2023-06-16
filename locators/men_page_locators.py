c134683bbef616c092dba68b76b8c391a6f70ede
gdanilova <130004322+gdanilova@users.noreply.github.com>
2023-06-15 20:37:56 -0400

    add Test 14.06.01 Verify the 'Men' header is displayed

--- locators/men_page_locators.py
+++ locators/men_page_locators.py
@@ -11,4 +11,6 @@ class MenPageLocators:
     SIDE_BAR_SUBHEAD_TOPS = (By.XPATH, '//*[@id="maincontent"]/div[4]/div[2]/div[2]/div/strong/span')
     SIDE_BAR_SUBHEAD_BOTTOMS = (By.CSS_SELECTOR, '#maincontent > div.sidebar > div.widget > div > strong:nth-child(3)')
     SIDE_BAR_HOODIES = (By.CSS_SELECTOR, 'div.sidebar-main ul.block-static-block li a')
-    SIDE_BAR_JACKETS = (By.CSS_SELECTOR, 'div.widget.block.block-static-block ul li a')
+    SIDE_BAR_JACKETS = (By.CSS_SELECTOR, '.widget.block.block-static-block ul li.item-2 a')
+    MEN_SUBHEAD_TEXT_JACKETS = (By.CSS_SELECTOR, '#page-title-heading > span')
+    SIDE_BAR_HEADER_MEN = (By.CSS_SELECTOR, '#page-title-heading > span')
