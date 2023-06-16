7eaf54c735a5a26495728508b2493e0f701c5a74
Galina Danilova <galya.danilova@gmail.com>
2023-06-16 00:12:56 -0400

    add TC 14.03.06 Verify the link 'Tees' is visible and clickable (#163)
    fix test number tc_14_03_05

--- tests/test_men.py
+++ tests/test_men.py
@@ -63,10 +63,18 @@ class TestMenPage:
         assert subhead_title == "Men"
 
     @allure.title("TC 14.03.05 Verify the link 'Jackets' redirects to a correct page.")
-    def test_tc_14_02_05(self, driver):
+    def test_tc_14_03_05(self, driver):
         """Verify that the link 'Jackets' correctly opens and redirects to a new webpage, the header 'Jackets' is
           displayed"""
         page = MenPage(driver, MEN_PAGE_URL)
         page.open()
         current_page = page.verify_jackets_redirects_to_a_correct_page()
         assert current_page, "New page isn't open or the 'Jackets' subhead is incorrect"
+
+    @allure.title("TC 14.03.06 Verify the link 'Tees' is visible and clickable.")
+    def test_tc_14_03_06(self, driver):
+        """Verify that the link 'Tees' is displayed and clickable"""
+        page = MenPage(driver, MEN_PAGE_URL)
+        page.open()
+        link = page.verify_tees_link_is_visible_and_clickable()
+        assert link, "The link 'Tees' is not visible"
