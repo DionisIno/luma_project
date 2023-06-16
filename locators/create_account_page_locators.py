5d27a38ea16aa1c7f5012f3ab1c07776d3a60865
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-06-16 23:15:05 +0400

    ref: header_page.py, create_account_page_locators.py, create_an_account_page.py, test_create_an_account.py add: test_tc_04_02_08, watches_page_locators.py

--- locators/create_account_page_locators.py
+++ locators/create_account_page_locators.py
@@ -14,4 +14,4 @@ class CreateAccountPageLocators:
     PASSWORD = (By.CSS_SELECTOR, '[id="password"]')
     PASSWORD_CONFIRMATION = (By.CSS_SELECTOR, '[id="password-confirmation"]')
     CREATE_AN_ACCOUNT_BUTTON = (By.CSS_SELECTOR, 'button[title="Create an Account"]')
-    MASSAGE_ERROR = By.CSS_SELECTOR, 'div[data-bind="html: $parent.prepareMessageForHtml(message.text)"]'
+    MASSAGE_ERROR = By.CSS_SELECTOR, '.message-error> div'
