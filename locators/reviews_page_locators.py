e4eed26329b954e36b23d6163c524c1bc15d1126
ILLIA LEVCHENKO <62564629+Dev0011111000101010@users.noreply.github.com>
2023-06-15 17:21:30 -0700

    Revert "Тест 4 звезды"
    This reverts commit b263808ac3c28e81d23615e242594c74350c9dd5.

--- locators/reviews_page_locators.py
+++ locators/reviews_page_locators.py
@@ -11,7 +11,6 @@ class ReviewsPageLocators:
     STAR_1 = (By.CSS_SELECTOR, '#Rating_1_label')
     STAR_2 = (By.CSS_SELECTOR, '#Rating_2_label')
     STAR_3 = (By.CSS_SELECTOR, '#Rating_3_label')
-    STAR_4 = (By.CSS_SELECTOR, '#Rating_4_label')
     NICKNAME_INPUT = (By.CSS_SELECTOR, '#nickname_field')
     SUMMARY_INPUT = (By.CSS_SELECTOR, '#summary_field')
     REVIEW_INPUT = (By.CSS_SELECTOR, '#review_field')
