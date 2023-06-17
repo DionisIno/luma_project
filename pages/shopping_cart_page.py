9d3b2f675d39472dc15fdd335c675c957b1e2dbf
Irina Vasilyeva <irishka.cdo@gmail.com>
2023-06-18 01:45:38 +0300

    add comment for shopping_cart_page

--- pages/shopping_cart_page.py
+++ pages/shopping_cart_page.py
@@ -8,16 +8,19 @@ class ShoppingCartPage(BasePage):
 
     @allure.step("Check shopping cart title")
     def check_shopping_cart_title(self):
+        """This method check that shopping cart title is displayed correctly"""
         shopping_cart_title = self.element_is_visible(self.shopping_locators.TITLE).text
         return shopping_cart_title
 
     @allure.step("Check here link is clickable")
     def check_here_link_is_clickable(self):
+        """This method check that here link in the displayed text is clickable"""
         here_link = self.element_is_clickable(self.shopping_locators.HERE_LINK)
         return here_link
 
     @allure.step("Check here link leads to correct url")
     def here_link_actual_url(self):
+        """This method check that here link in the displayed text leads to the correct url"""
         here_link = self.element_is_visible(self.shopping_locators.HERE_LINK)
         here_link.click()
         current_url = self.driver.current_url
