affeeb36a3efe1e266c99bb73fcd8cdf5ec7f170
Natalya Mastyaeva <71374230+Nat754@users.noreply.github.com>
2023-06-17 21:48:30 +0400

    ref: header_page.py, create_account_page_locators.py, create_an_accou… (#159)
    add: test_tc_04_02_08

--- locators/create_account_page_locators.py
+++ locators/create_account_page_locators.py
@@ -7,3 +7,11 @@ class CreateAccountPageLocators:
     # Page-title Wrapper
     CREATE_NEW_CUSTOMER_ACCOUNT_HEADER = (By.CSS_SELECTOR, "h1")
 
+    # Create an Account
+    FIRST_NAME = (By.ID, 'firstname')
+    LAST_NAME = (By.ID, 'lastname')
+    EMAIL = (By.ID, 'email_address')
+    PASSWORD = (By.CSS_SELECTOR, '[id="password"]')
+    PASSWORD_CONFIRMATION = (By.CSS_SELECTOR, '[id="password-confirmation"]')
+    CREATE_AN_ACCOUNT_BUTTON = (By.CSS_SELECTOR, 'button[title="Create an Account"]')
+    MESSAGE_ERROR = (By.CSS_SELECTOR, '.message-error.error.message > div')
