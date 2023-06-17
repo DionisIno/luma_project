8f4c35291f2c89a91b1d7cad4d6c0d27a2b71c2e
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-06-17 18:22:01 +0400

    ref: header_page.py, test_training_video_download.py, training_video_download_page.locators.py, training_video_download_page.py add: test_tc_18_01_02

--- locators/training_video_download_page_locators.py
+++ locators/training_video_download_page_locators.py
@@ -7,3 +7,4 @@ class TrainingVideoDownloadPageLocators:
 
     HEAD_TEXT = (By.CSS_SELECTOR, '[data-ui-id="page-title-wrapper"]')
     COMPARE_PRODUCTS = (By.ID, 'block-compare-heading')
+    MY_WISH_LIST = (By.CSS_SELECTOR, '.block.block-wishlist .block-title')
