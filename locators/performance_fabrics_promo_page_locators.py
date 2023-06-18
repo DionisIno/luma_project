b75a270e5204248171335b4d58f6ca9e97b085f9
Elena <peregovorun@gmail.com>
2023-06-18 14:37:35 +0300

    add performance_fabrics_promo_page_locators.py

---
+++ locators/performance_fabrics_promo_page_locators.py
@@ -0,0 +1,8 @@
+"""This section contains locators for Performance Fabrics promo page"""
+
+from selenium.webdriver.common.by import By
+
+
+class PerformanceFabricsPromoPageLocators:
+    PERFORMANCE_FABRICS_PROMO_TITLE = (By.CSS_SELECTOR, '#page-title-heading .base')
+
