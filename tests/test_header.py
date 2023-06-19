818ed11215a251c7a0bcce44fa2d4d82fbf506ee
badx86 <badlolpro@gmail.com>
2023-06-19 20:17:09 +0400

    add tc_01_01_10

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,7 +1,7 @@
 import allure
 from data.data_urls import MAIN_PAGE_URL, CREATE_ACCOUNT_PAGE_URL, SIGN_IN_URL, MEN_BOTTOMS_URL, \
     MEN_BOTTOMS_PANTS_URL, MEN_BOTTOMS_SHORTS_URL, MEN_PAGE_URL, MEN_TOPS_URL, MEN_JACKETS_URL, MEN_HOODIES_URL, \
-    MEN_TEES_URL, MEN_TANKS_URL
+    MEN_TEES_URL, MEN_TANKS_URL, MY_ACCOUNT_URL
 from pages.header_page import HeaderPage
 
 
@@ -343,4 +343,16 @@ class TestHeader:
         page.check_redirection_of_tanks_subsection_of_men_section_link()
         header = page.check_common_header()
         assert driver.current_url == MEN_TANKS_URL and header.text == "Tanks", \
+            "Tanks-men page of Men section is either not opened or the page header is incorrect"
+
+    def test_tc_01_01_10_functionality_of_the_my_account_section_in_the_dropdown_list(self, driver):
+        """Check the list appears with sections 'My Account', 'My Wish List', 'Sign Out' after clicking the button,\
+        then clicking on 'My Account' redirects to the account page with the header 'My Account' displayed"""
+        page = HeaderPage(driver, MAIN_PAGE_URL)
+        page.open()
+        page.user_authorization()
+        page.click_and_return_element(page.header_locators.DROPDOWN_BUTTON)
+        page.check_redirection_of_the_my_account_link()
+        header = page.check_common_header()
+        assert driver.current_url == MY_ACCOUNT_URL and header.text == "My Account", \
             "Tanks-men page of Men section is either not opened or the page header is incorrect"
