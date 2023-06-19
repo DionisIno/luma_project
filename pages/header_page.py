10d93a7144bbf6fcd852c15ea9442986f356f015
Stanislav_Osipov <badlolpro@gmail.com>
2023-06-19 18:38:43 +0400

    add tc_01_03_21 (#194)

--- pages/header_page.py
+++ pages/header_page.py
@@ -282,3 +282,14 @@ class HeaderPage(BasePage):
         self.action_move_to_element(self.element_is_visible(self.header_locators.JACKETS_SUBSECTION))
         self.element_is_visible(self.header_locators.HOODIES_SWEATSHIRTS_SUBSECTION).click()
         return self.element_is_visible(self.common_locators.HEADER_PAGE)
+
+    @allure.step('Check redirection of the "Hoodies & Sweatshirts" subsection of "Men" section link')
+    def check_redirection_of_tees_subsection_of_men_section_link(self):
+        """This method checks the redirection of the 'Tees' subsection link of the 'Men' section.
+        It moves the cursor over the 'Men' section, then 'Tops' and clicks on the 'Tees' subsection link,
+        and returns the visibility of the header page element"""
+        self.action_move_to_element(self.element_is_visible(self.header_locators.MEN_SECTION))
+        self.action_move_to_element(self.element_is_visible(self.header_locators.TOPS_SUBSECTION))
+        self.action_move_to_element(self.element_is_visible(self.header_locators.JACKETS_SUBSECTION))
+        self.element_is_visible(self.header_locators.TEES_SUBSECTION).click()
+        return self.element_is_visible(self.common_locators.HEADER_PAGE)
