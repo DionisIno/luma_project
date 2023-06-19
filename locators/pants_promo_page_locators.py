d8d2e324ce9f585b83060fb6897bc127c43e8019
NKrap <129363326+NKrap@users.noreply.github.com>
2023-06-19 16:28:17 +0500

    Add: TC_19.01.01 - 19.01.02 tests for checking titles of Shopping Options and Style are correct (#189)
    * Added tests for checking titles of Shopping Options and Style are corrected
    
    * Ref: Added selectors in pents_promo_page_locators. deleted file promotion_pants_page_locators.py

--- locators/pants_promo_page_locators.py
+++ locators/pants_promo_page_locators.py
@@ -5,3 +5,5 @@ from selenium.webdriver.common.by import By
 
 class PantsPromoPageLocators:
     PANTS_PROMO_TITLE = (By.CSS_SELECTOR, '#page-title-heading .base')
+    SHOPPING_OPTIONS = (By.XPATH, "//strong[text()='Shopping Options']")
+    STYLE = (By.XPATH, "//div[text()='Style']")
