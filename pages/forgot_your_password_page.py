3fcb4e4f8527e75b4b86eaf1e1bcc5313e7d4705
Vera Fesianava <verafes@gmail.com>
2023-06-22 15:41:42 -0700

    Add: TC 17.01.06,07 FYP Email, 2 base methods (#221)

--- pages/forgot_your_password_page.py
+++ pages/forgot_your_password_page.py
@@ -13,24 +13,42 @@ class ForgotYourPasswordPage(BasePage):
         '''This method verifies if h1 header is visible on the page'''
         return self.element_is_visible(self.locators.PAGE_HEADER)
 
-    @allure.step('CheckForgot Your Password note is visible')
+    @allure.step('Check Forgot Your Password note is visible')
     def check_forgot_your_psw_note(self):
         """This method verifies if note is visible under heading"""
         return self.element_is_visible(self.locators.FORGOT_YOUR_PASSWORD_NOTE)
 
-    @allure.step('Check Email label is visible')
+    @allure.step('Check FYP Email label is visible')
     def check_forgot_psw_email_label(self):
         """This method verifies if Email label is visible"""
         return self.element_is_visible(self.locators.EMAIL_LABEL)
 
-    @allure.step('Check Email asterisk is visible')
+    @allure.step('Check FYP Email asterisk is visible')
     def check_forgot_psw_email_asterisk(self):
         """This method verifies if asterisk is displayed next to Email label"""
         email_label = self.check_forgot_psw_email_label()
         asterisk_script = self.find_required_element()
         return self.driver.execute_script(asterisk_script, email_label)
 
-    @allure.step('Check Email field is clickable')
+    @allure.step('Check FYP Email field is clickable')
     def check_forgot_psw_email_field_is_clickable(self):
         """This method verifies if Email field is clickable"""
         return self.element_is_clickable(self.locators.EMAIL)
+
+    @allure.step('Activate FYP Email field and check style')
+    def activate_forgot_psw_email_field_and_check_style(self):
+        """
+        This method activates the Email field
+        and checks if the style of the field changes upon activation.
+        """
+        return self.activate_field_and_check_style(self.locators.EMAIL)
+
+    @allure.step('Fill in FYP Email field')
+    def fill_in_forgot_psw_email_field(self, email):
+        """This method fills in the Email field with the provided email"""
+        return self.fill_in_field(self.check_forgot_psw_email_field_is_clickable(), email)
+
+    @allure.step('Get FYP Email field attribute')
+    def get_forgot_psw_email_field_attribute(self, attribute):
+        """This method gets the entered value from Email field"""
+        return self.check_forgot_psw_email_field_is_clickable().get_attribute(attribute)
