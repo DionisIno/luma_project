30bf21c25501bb94df3bc6ed1235d88b65cdf8db
Alena Krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-06-26 23:06:02 +0300

    add TC_02.01.05 (#238)
    add TC_02.01.10
    add TC_02.01.15
    add TC_02.01.20
    add TC_02.01.25
    add TC_02.01.30

--- pages/footer_page.py
+++ pages/footer_page.py
@@ -46,11 +46,41 @@ class FooterPage(BasePage):
 
     @allure.step("Check Footer is visible on the page")
     def check_footer_is_visible(self):
-        """Checks Footer is visible on the page"""
+        """Checks Footer is visible"""
         return self.element_is_visible(self.footer_locators.FOOTER_SECTION)
 
     @allure.step("Check Search Terms link is visible on the page")
     # def check_search_terms_link_is_visible(self, expected_link):
     def check_search_terms_link_is_visible(self):
-        """Checks Search Terms link is visible on the page"""
+        """Checks Search Terms link is visible"""
         return self.element_is_visible(self.footer_locators.SEARCH_TERMS_LINK)
+
+    @allure.step("Check Search Terms link is clickable")
+    def check_search_terms_link_clickability(self):
+        """Checks Search Terms link clickability"""
+        return self.element_is_clickable(self.footer_locators.SEARCH_TERMS_LINK)
+
+    @allure.step("Check Privacy and Cookie Policy link is clickable")
+    def check_privacy_and_cookie_policy_link_clickability(self):
+        """Checks Privacy and Cookie Policy link clickability"""
+        return self.element_is_clickable(self.footer_locators.PRIVACY_AND_COOKIE_POLICY_LINK)
+
+    @allure.step("Check Advanced Search link is clickable")
+    def check_advanced_search_link_clickability(self):
+        """Checks Advanced Search link clickability"""
+        return self.element_is_clickable(self.footer_locators.ADVANCED_SEARCH_LINK)
+
+    @allure.step("Check Orders and Returns link is clickable")
+    def check_orders_and_returns_link_clickability(self):
+        """Checks Orders and Returns link clickability"""
+        return self.element_is_clickable(self.footer_locators.ORDERS_AND_RETURNS_LINK)
+
+    @allure.step("Check Contact Us link is clickable")
+    def check_contact_us_link_clickability(self):
+        """Checks Contact Us link clickability"""
+        return self.element_is_clickable(self.footer_locators.CONTACT_US_LINK)
+
+    @allure.step("Check Write for us link is clickable")
+    def check_write_for_us_link_clickability(self):
+        """Checks Write for us link clickability"""
+        return self.element_is_clickable(self.footer_locators.WRITE_FOR_US_LINK)
