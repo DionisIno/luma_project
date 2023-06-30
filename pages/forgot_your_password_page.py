0aff51875a09981a754c6cd4dcbf988ab1f1ea31
verafes <verafes@gmail.com>
2023-06-29 21:04:24 -0700

    Add: TC 17.01.11,12,13 captcha input field

--- pages/forgot_your_password_page.py
+++ pages/forgot_your_password_page.py
@@ -23,7 +23,7 @@ class ForgotYourPasswordPage(BasePage):
         """This method verifies if Email label is visible"""
         return self.element_is_visible(self.locators.EMAIL_LABEL)
 
-    @allure.step('Check FYP Email asterisk is visible')
+    @allure.step('Check FYP Email asterisk is displayed')
     def check_forgot_psw_email_asterisk(self):
         """This method verifies if asterisk is displayed next to Email label"""
         email_label = self.check_forgot_psw_email_label()
@@ -57,3 +57,33 @@ class ForgotYourPasswordPage(BasePage):
     def check_forgot_psw_captcha_field_is_clickable(self):
         """This method verifies if captcha field is clickable"""
         return self.element_is_clickable(self.locators.FORGOT_YOUR_PASSWORD_CAPTCHA)
+
+    @allure.step('Check the label for FYP captcha input field is visible')
+    def check_forgot_psw_captcha_label(self):
+        """This method verifies if captcha label is visible"""
+        return self.element_is_visible(self.locators.FORGOT_YOUR_PASSWORD_CAPTCHA_LABEL)
+
+    @allure.step('Check asterisk for FYP Captcha input field label is displayed')
+    def check_forgot_psw_captcha_asterisk(self):
+        """This method verifies if asterisk is displayed next to captcha label"""
+        captcha_label = self.check_forgot_psw_captcha_label()
+        asterisk_script = self.find_required_element()
+        return self.driver.execute_script(asterisk_script, captcha_label)
+
+    @allure.step('Activate FYP Captcha input field and check style')
+    def activate_forgot_psw_captcha_field_and_check_style(self):
+        """
+        This method activates the captcha field
+        and checks if the style of the field changes upon activation.
+        """
+        return self.activate_field_and_check_style(self.locators.FORGOT_YOUR_PASSWORD_CAPTCHA)
+        
+    @allure.step('Fill in FYP Email field')
+    def fill_in_forgot_psw_captcha_field(self, text):
+        """This method fills in the captcha field with the provided email"""
+        return self.fill_in_field(self.check_forgot_psw_captcha_field_is_clickable(), text)
+
+    @allure.step('Get FYP Captcha input field attribute')
+    def get_forgot_psw_captcha_field_attribute(self, attribute):
+        """This method gets the entered value from captcha field"""
+        return self.check_forgot_psw_captcha_field_is_clickable().get_attribute(attribute)
