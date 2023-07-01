359ca8093b389a2ccfa0eea46a9f14c030a284b4
Stanislav_Osipov <badlolpro@gmail.com>
2023-07-01 18:36:48 +0400

    add tc_12.05.01, tc_12.05.02, tc_12.05.03, tc_12.05.04 (#260)

--- locators/women_page_locators.py
+++ locators/women_page_locators.py
@@ -8,3 +8,12 @@ class WomenPageLocators:
     SHOP_BY = (By.XPATH, '//div[4]/div[2]/div[1]/div[1]/strong')
     WOMEN_HEAD_TEXT = (By.XPATH, '//*[@id="page-title-heading"]/span')
     CATEGORY_TITLE = (By.XPATH, '//div[4]/div[2]/div[1]/div[2]/dl/dt')
+    # Sidebar - additional
+    COMPARE_PRODUCTS = (By.XPATH, "//strong[@id='block-compare-heading']")
+    NOTE_1 = (By.XPATH, "// div[normalize-space() = 'You have no items to compare.']")
+    MY_WISH_LIST = (By.XPATH, "//strong[normalize-space()='My Wish List']")
+    NOTE_2 = (By.XPATH, "//div[normalize-space()='You have no items in your wish list.']")
+    ITEMS = [(COMPARE_PRODUCTS, "Compare Products"),
+             (NOTE_1, "You have no items to compare."),
+             (MY_WISH_LIST, "My Wish List"),
+             (NOTE_2, "You have no items in your wish list.")]
--- tests/test_women.py
+++ tests/test_women.py
@@ -1,6 +1,8 @@
 from data.data_urls import WOMEN_PAGE_URL
 from pages.women_page import WomenPage
+from locators.women_page_locators import WomenPageLocators
 import allure
+import pytest
 
 
 @allure.epic("WomenPage")
@@ -21,3 +23,13 @@ class TestWomenPage:
         page.open()
         title = page.check_side_panel_category_is_visible()
         assert title == "Category", f"Expected title: 'Category, Actual title: {title}"
+
+    @allure.title("TC 12.05.01/02/03/04 Check the Sidebar text is visible")
+    @pytest.mark.parametrize("item_locator, expected_text", WomenPageLocators.ITEMS)
+    def test_item_visibility_and_text(self, driver, item_locator, expected_text):
+        """Check if items are visible"""
+        page = WomenPage(driver, WOMEN_PAGE_URL)
+        page.open()
+        element = page.element_is_visible(item_locator)
+        assert element is not None, "Element is not visible"
+        assert element.text == expected_text, f"Expected text is '{expected_text}', but got '{element.text}'"
