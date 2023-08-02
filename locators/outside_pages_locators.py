e69fb4af58a847475ddd0f94075f57b4f482d6ec
Alena Krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-08-02 23:57:36 +0300

    add 02.02.07 (#313)
    update data_urls.py
    update footer_page.py
    update outside_pages_locators.py

--- locators/outside_pages_locators.py
+++ locators/outside_pages_locators.py
@@ -5,3 +5,7 @@ from selenium.webdriver.common.by import By
 
 class WriteForUsPageLocators:
     WRITE_FOR_US_MENU_ITEM = (By.CSS_SELECTOR, '#menu-item-7639 > a')
+
+
+class SubscribePageLocators:
+    SUBSCRIBE_TITLE = (By.CSS_SELECTOR, '#mc_embed_signup_scroll > h2')
