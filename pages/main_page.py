465686747cd8de144cbeb541b44e36abf78c91e2
Alena Krauch <94075420+alena-krauchuk@users.noreply.github.com>
2023-10-08 13:10:13 +0300

    feat: ref tc_13.01.19, tc_13.01.20 (#339)
    update test_main_page.py, main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -249,9 +249,9 @@ class PromoBlock(BasePage):
     def check_image_src(self, element):
         return element.get_attribute("src")
 
-    @allure.step("Get the element text")
-    def get_element_text(self, element):
-        return element.text
+    # @allure.step("Get the element text")
+    # def get_element_text(self, element):
+    #     return element.text
 
     @allure.step("Check the image in section 2 block 5 'home-eco' is correct in the Promo Block")
     def check_image_in_section2_block5(self):
@@ -289,12 +289,6 @@ class PromoBlock(BasePage):
         """Checks the title of info-block in block 5 'home-eco'"""
         info_block_title = self.find_element(self.locators.SECTION_2_BLOCK_5_INFO_BLOCK_TITLE).text
         return info_block_title
-
-    @allure.step("Check the text of info block in section 2 block 1 'home-pants' in the Promo Block is correct")
-    def check_info_block_text_in_section2_block1(self):
-        """Checks the text of info-block in block 1 'home-pants'"""
-        info_block_text = self.find_element(self.locators.SECTION_2_BLOCK_1_INFO_BLOCK_TEXT).text
-        return info_block_text
 
     @allure.step("Check the text of info block in section 2 block 2 'home-t-shirts' in the Promo Block is correct")
     def check_info_block_text_in_section2_block2(self):
