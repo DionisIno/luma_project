bd3c7a1fba1364449bca77f453ef5f6d40640abe
verafes <verafes@gmail.com>
2024-01-19 22:26:32 -0800

    fix: footer locators

--- locators/contact_us_page_locators.py
+++ locators/contact_us_page_locators.py
@@ -6,4 +6,4 @@ from selenium.webdriver.common.by import By
 
 class ContactUsPageLocators:
     CONTACT_US_TITLE = (By.CSS_SELECTOR, '.page-title .base')           # css-selector for old page
-    CONTACT_TITLE = (By.CSS_SELECTOR, '.elementor-element-94d10e2 h2')  # css-selector for new page
+    CONTACT_TITLE = (By.CSS_SELECTOR, '.elementor-element-94d10e2 > div h2')  # updated 01.20.2024
--- locators/footer_page_locators.py
+++ locators/footer_page_locators.py
@@ -5,16 +5,16 @@ from selenium.webdriver.common.by import By
 
 class FooterPageLocators:
     ADVANCED_SEARCH_LINK = (By.CSS_SELECTOR, '.footer > :nth-child(3) > a')
-    CONTACT_US_LINK = (By.CSS_SELECTOR, '.footer div :nth-child(3) a')
+    CONTACT_US_LINK = (By.CSS_SELECTOR, '.footer div :nth-child(4) a')                      # updated 01.20.2024
     COPYRIGHT_SECTION = (By.CSS_SELECTOR, '.copyright')
     COPYRIGHT_TEXT = (By.CSS_SELECTOR, '.copyright > span')
     FOOTER_SECTION = (By.CSS_SELECTOR, '.page-footer')
     FOOTER_CONTENT = (By.CSS_SELECTOR, '.footer.content')
     ORDERS_AND_RETURNS_LINK = (By.CSS_SELECTOR, '.footer > :nth-child(4) > a')
     PRIVACY_AND_COOKIE_POLICY_LINK = (By.CSS_SELECTOR, '.footer > :nth-child(2) > a')
-    SEARCH_TERMS_LINK = (By.CSS_SELECTOR, ".footer > :nth-child(1) > a")
-    SUBSCRIBE_TO_OUR_MAILING_LIST_LINK = (By.CSS_SELECTOR, '.footer div :nth-child(2) a')
-    WRITE_FOR_US_LINK = (By.CSS_SELECTOR, '.footer div ul :nth-child(1) a')
+    SEARCH_TERMS_LINK = (By.CSS_SELECTOR, ".footer > :nth-child(2) > a")                    # updated 01.20.2024
+    SUBSCRIBE_TO_OUR_MAILING_LIST_LINK = (By.CSS_SELECTOR, '.footer div :nth-child(3) a')   # updated 01.20.2024
+    WRITE_FOR_US_LINK = (By.CSS_SELECTOR, '.footer div ul :nth-child(2) a')                 # updated 01.20.2024
     # SUBSCRIBE_BUTTON = (By.CSS_SELECTOR, '.block.newsletter .actions')                    # excluded from interface
     # SUBSCRIBE_BUTTON_TEXT = (By.CSS_SELECTOR, '.block.newsletter .actions span')          # excluded from interface
     # SUBSCRIBE_EMAIL_FIELD = (By.CSS_SELECTOR, '.field.newsletter input')                  # excluded from interface
